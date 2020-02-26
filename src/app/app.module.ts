765f4f1cc5a3343c00e016bda401ffd66ab4f53b
KevinR117 <59045597+KevinR117@users.noreply.github.com>
2020-02-26 10:39:46 +0100

    Ajout d'un component pour gérer le routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppareilComponent } from './appareil/appareil.component';
 import { FormsModule } from '@angular/forms';
 import { PostsComponent } from './posts/posts.component';
 import { PostsService } from './services/posts.service';
+import { AuthComponent } from './auth/auth.component';
+import { PostsViewComponent } from './posts-view/posts-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent
+    PostsComponent,
+    AuthComponent,
+    PostsViewComponent
   ],
   imports: [
     BrowserModule,
